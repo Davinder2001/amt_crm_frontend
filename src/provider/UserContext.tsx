559a910b9a405f3ccc3e6e199fc0731661f7b5d0
Davinder <162427407+Davinder2001@users.noreply.github.com>
2025-03-20 16:55:25 +0530

    updated code

--- src/provider/UserContext.tsx
+++ src/provider/UserContext.tsx
@@ -3,16 +3,42 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { useFetchProfileQuery } from '@/slices/auth/authApi';
 
+interface Company {
+  id: number;
+  admin_id: number;
+  company_id: string;
+  company_name: string;
+  company_slug: string;
+  created_at: string;
+  payment_status: string;
+  updated_at: string;
+  verification_status: string;
+  [key: string]: string | number;
+}
+
+interface User {
+  id: number;
+  name: string;
+  number: string;
+  company_id: number;
+  company_name: string;
+  company_slug: string;
+  meta: { [key: string]: string | number | boolean | object };
+  user_type: "admin" | "employee" | "user" | "superadmin";
+  password: string;
+  companies: Company[];
+}
+
 type UserContextType = {
-  user: any;
-  setUser: (user: any) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
 };
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const { data } = useFetchProfileQuery();
-  const [user, setUser] = useState(data?.user || null);
+  const [user, setUser] = useState<User | null>(data?.user || null);
 
   useEffect(() => {
     if (data?.user) {

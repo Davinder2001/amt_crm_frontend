d5eaae38ac8b1d24b0b2c148dcbc3dc3537665f6
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-03-26 18:29:50 +0530

    changes

--- src/layouts/SuperAdminLayout.tsx
+++ src/layouts/SuperAdminLayout.tsx
@@ -2,19 +2,24 @@
 
 import Header from "@/app/superadmin/components/header";
 import SideBar from "@/app/superadmin/components/sideBar";
+import { usePathname } from "next/navigation";
 
 interface SuperAdminLayoutProps {
   children: React.ReactNode;
 }
 
 export const SuperAdminLayout: React.FC<SuperAdminLayoutProps> = ({ children }) => {
-    return (
-      <div className="super-admin-layout-wrapper">
-        <div className="side-bar">
-          <SideBar/>
-        </div>
-        <Header/>
-        <main className="super-admin-layout">{children}</main>
+  const pathname = usePathname()
+  if (pathname === '/') {
+    return <>{children}</>
+  }
+  return (
+    <div className="super-admin-layout-wrapper">
+      <div className="side-bar">
+        <SideBar />
       </div>
-    );
+      <Header />
+      <main className="super-admin-layout">{children}</main>
+    </div>
+  );
 }

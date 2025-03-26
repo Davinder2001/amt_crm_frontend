1fbc639087937e042aa2ab9bc0234400e7426bc0
Davinder <162427407+Davinder2001@users.noreply.github.com>
2025-03-26 12:24:33 +0530

    sidebar added in super admin

--- src/layouts/SuperAdminLayout.tsx
+++ src/layouts/SuperAdminLayout.tsx
@@ -1,14 +1,21 @@
 'use client'
 
+import Header from "@/app/superadmin/components/header";
+import SideBar from "@/app/superadmin/components/sideBar";
+
 interface SuperAdminLayoutProps {
   children: React.ReactNode;
 }
 
 const SuperAdminLayout: React.FC<SuperAdminLayoutProps> = ({ children }) => {
     return (
-      <>
+      <div className="">
+        <div className="side-bar">
+          <SideBar/>
+        </div>
+        <Header/>
         <main className="super-admin-layout">{children}</main>
-      </>
+      </div>
     );
 }
 

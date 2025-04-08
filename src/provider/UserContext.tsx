2018bb57d7a9de943a2c3c022ce8a72dfaea9a0e
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-04-08 19:04:02 +0530

    contexts changes

--- src/provider/UserContext.tsx
+++ src/provider/UserContext.tsx
@@ -2,6 +2,8 @@
 
 import { createContext, useContext, useEffect, useState } from 'react';
 import { useFetchProfileQuery } from '@/slices/auth/authApi';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 type UserContextType = {
   user: UserProfile | null;
@@ -11,14 +13,30 @@ type UserContextType = {
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
-  const { data } = useFetchProfileQuery();
+  const { data, error} = useFetchProfileQuery();
   const [user, setUser] = useState<UserProfile | null>(data?.user || null);
-  
+  const router = useRouter();
+
   useEffect(() => {
     if (data?.user) {
       setUser(data.user);
     }
-  }, [data, user]);
+  }, [data, user, setUser]);
+
+  // useEffect(() => {
+  //   if (data?.message === "Unauthenticated.") {
+  //     // Clear the user context
+  //     setUser(null);
+
+  //     // Remove cookies
+  //     Cookies.remove('access_token');
+  //     Cookies.remove('user_type');
+  //     Cookies.remove('company_slug');
+
+  //     // Redirect to login page
+  //     router.push('/login');
+  //   }
+  // }, [user, data, error, router])
 
   return (
     <UserContext.Provider value={{ user, setUser }}>

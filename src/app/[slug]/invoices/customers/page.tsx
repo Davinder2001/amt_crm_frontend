9d50871ac364db271cbab15130d2210b45f1517c
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-04-09 18:47:52 +0530

    changes

--- src/app/[slug]/invoices/customers/page.tsx
+++ src/app/[slug]/invoices/customers/page.tsx
@@ -1,16 +1,40 @@
 'use client';
 import { useFetchAllCustomersQuery } from '@/slices/customers/customer';
-import React from 'react'
+import React from 'react';
+import { FaEye } from 'react-icons/fa'; // Import the 'eye' icon for view action
 
-const Page = () => {
-  const {data: customers} = useFetchAllCustomersQuery();
-  console.log('customers.....', customers);
+const CustomerList = () => {
+  const { data } = useFetchAllCustomersQuery();
 
   return (
-    <>
+    <div>
       <h1>All Customers</h1>
-    </>
-  )
-}
+      <table cellPadding="10" cellSpacing="0" style={{ width: '100%' }}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Phone Number</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.customers?.map((customer) => (
+            <tr key={customer.id}>
+              <td>{customer.id}</td>
+              <td>{customer.name}</td>
+              <td>{customer.number}</td>
+              <td>
+                <button onClick={() => alert(`Viewing customer ${customer.name}`)}>
+                  <FaEye /> View
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Page
+export default CustomerList;

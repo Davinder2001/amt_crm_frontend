41746671ccd70000a6a0e8184a8fbeb651e71fb6
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-04-10 16:55:43 +0530

    make seperate layout for admin and employee

--- src/app/[slug]/_common/sidebar/sidebar.tsx
+++ src/app/[slug]/_common/sidebar/sidebar.tsx
@@ -16,7 +16,7 @@ interface sidebarProps {
 }
 
 const Sidebar: React.FC<sidebarProps> = ({ isSidebarExpanded, isMobile, openMenu }) => {
-  const { companySlug } = useCompany();
+  const { companySlug, userType } = useCompany();
 
   // Using useRouter to get the current route
   const asPath = usePathname();
@@ -75,7 +75,7 @@ const Sidebar: React.FC<sidebarProps> = ({ isSidebarExpanded, isMobile, openMenu
                 }}
                 onClick={openMenu}
               >
-                <Link href={`/${companySlug}/${path}`} className="menu-link">
+                <Link href={`/${companySlug}${userType === 'employee' ? '/employee' : ''}/${path}`} className="menu-link">
                   <span className="menu-icon"
                     style={{
                       color: isActive ? "#009693" : "#222",

8c7758405d88031e0e02483f03a7a966b615f85f
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-04-10 17:53:50 +0530

    added employee and admin navs

---
+++ src/app/[slug]/_common/sidebar/AdminNavs.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { FaTachometerAlt, FaStore, FaUserTie, FaUserShield, FaCog, FaTasks, FaFileInvoice, FaClipboardList } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { useCompany } from "@/utils/Company";
+
+const AdminNavs: React.FC = () => {
+    const asPath = usePathname();
+    const { companySlug } = useCompany();
+    const menuItems = [
+        { name: "Dashboard", path: "dashboard", icon: <FaTachometerAlt /> },
+        { name: "Catalogue", path: "catalogue", icon: <FaClipboardList /> },
+        { name: "Store", path: "store", icon: <FaStore /> },
+        // { name: "Services", path: "services", icon: <LuClipboardList /> },
+        { name: "H.R", path: "hr", icon: <FaUserTie /> },
+        { name: "Invoices", path: "invoices", icon: <FaFileInvoice />, hasSubmenu: true },
+        { name: "Task", path: "tasks", icon: <FaTasks /> },
+        // { name: "Vehicle", path: "vehicle", icon: <FaCar /> },
+        // { name: "Quality Control", path: "quality-control", icon: <FaCheck /> },
+        { name: "Permissions", path: "permissions", icon: <FaUserShield /> },
+        // { name: "Orders", path: "orders", icon: <FaBox /> },
+        { name: "Settings", path: "settings", icon: <FaCog /> },
+    ];
+
+    return (
+        <nav>
+            <ul className="menu-list">
+                {menuItems.map(({ name, path, icon, hasSubmenu }) => {
+                    const isActive = asPath.includes(path);
+                    return (
+                        <li
+                            key={path}
+                            className={`menu-item ${hasSubmenu ? "has-submenu" : ""}`}
+                            style={{ backgroundColor: isActive ? "#F1F9F9" : "", position: "relative" }}
+                        >
+                            <Link href={`/${companySlug}/${path}`} className="menu-link">
+                                <span className="menu-icon" style={{ color: isActive ? "#009693" : "#222" }}>
+                                    {icon}
+                                </span>
+                                <span className="menu-text">{name}</span>
+                                {hasSubmenu && <span className="submenu-icon">+</span>}
+                            </Link>
+                            {isActive && (
+                                <span
+                                    style={{
+                                        position: "absolute",
+                                        top: 0,
+                                        right: 0,
+                                        width: "3px",
+                                        height: "100%",
+                                        backgroundColor: "#009693",
+                                        borderRadius: '2px'
+                                    }}
+                                />
+                            )}
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+};
+
+export default AdminNavs;
---
+++ src/app/[slug]/_common/sidebar/EmployeeNavs.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaTachometerAlt, FaClipboardList, FaTasks, FaUserTie } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { useCompany } from "@/utils/Company";
+
+const EmployeeNavs: React.FC = () => {
+    const asPath = usePathname();
+    const { companySlug } = useCompany();
+    const menuItems = [
+        { name: "Dashboard", path: "dashboard", icon: <FaTachometerAlt /> },
+        { name: "Catalogue", path: "catalogue", icon: <FaClipboardList /> },
+        { name: "Task", path: "tasks", icon: <FaTasks /> },
+        { name: "H.R", path: "hr", icon: <FaUserTie /> },
+    ];
+
+    return (
+        <nav>
+            <ul className="menu-list">
+                {menuItems.map(({ name, path, icon }) => {
+                    const isActive = asPath.includes(path);
+                    return (
+                        <li
+                            key={path}
+                            className="menu-item"
+                            style={{ backgroundColor: isActive ? "#F1F9F9" : "", position: "relative" }}
+                        >
+                            <Link href={`/${companySlug}/employee/${path}`} className="menu-link">
+                                <span className="menu-icon" style={{ color: isActive ? "#009693" : "#222" }}>
+                                    {icon}
+                                </span>
+                                <span className="menu-text">{name}</span>
+                            </Link>
+                            {isActive && (
+                                <span
+                                    style={{
+                                        position: "absolute",
+                                        top: 0,
+                                        right: 0,
+                                        width: "3px",
+                                        height: "100%",
+                                        backgroundColor: "#009693",
+                                        borderRadius: '2px'
+                                    }}
+                                />
+                            )}
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+};
+
+export default EmployeeNavs;

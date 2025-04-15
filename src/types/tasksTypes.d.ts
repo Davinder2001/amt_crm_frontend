1c146b6440429f1251c3fe59739f7be945d174ab
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-04-15 18:12:33 +0530

    resolve build errors

--- src/types/tasksTypes.d.ts
+++ src/types/tasksTypes.d.ts
@@ -5,6 +5,17 @@ interface Task {
     assigned_by_name: string;
     assigned_to_name: string;
     deadline: string;
+    assigned: string;
+    assigned_role: string;
+    start_date: string;
+    end_date: string;
+    status: string;
+    description: string;
+    company_id: string | number;
+    notify: boolean;
+    attachment_url: string;
+    created_at: string;
+    updated_at: string;
 }
 
 interface TasksResponse {

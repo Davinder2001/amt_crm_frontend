32cc70e7ab9e5b9b90ef19fd468f6c9290c37cab
Davinder <162427407+Davinder2001@users.noreply.github.com>
2025-04-18 17:19:07 +0530

    invoice added

--- src/app/[slug]/employee/invoices/page.tsx
+++ src/app/[slug]/employee/invoices/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useBreadcrumb } from '@/provider/BreadcrumbContext';
 import React, { useEffect } from 'react'
-import AllInvoices from './components/allInvoices';
+// import AllInvoices from './components/allInvoices';
 import InvoicesNavigation from './components/invoicesNavigation';
 
 function Page() {
@@ -15,7 +15,7 @@ function Page() {
       <div className="navigation">
         <InvoicesNavigation />
       </div>
-      <AllInvoices />
+      {/* <AllInvoices /> */}
     </>
   )
 }

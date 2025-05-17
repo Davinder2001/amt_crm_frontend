027bb78b37db5e52c750d48810912a959bbb3928
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-05-17 14:29:19 +0530

    update code

--- src/app/[slug]/my-account/add-company/page.tsx
+++ src/app/[slug]/my-account/add-company/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useFetchBusinessCategoriesQuery, useFetchPackagesPlansQuery } from '@/slices/users/userApi';
 import Loader from '@/components/common/Loader';
 import AddCompanyForm from '../components/addCompanyForm'

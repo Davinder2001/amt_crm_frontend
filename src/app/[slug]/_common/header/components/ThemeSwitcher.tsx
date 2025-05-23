1db783dd662b11d43d2e368a0cdaa88703dbc000
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-05-23 15:06:53 +0530

    added theme

--- src/app/[slug]/_common/header/components/ThemeSwitcher.tsx
+++ src/app/[slug]/_common/header/components/ThemeSwitcher.tsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState, AppDispatch } from '@/store/store';
 import { THEMES, type Theme } from '@/slices/theme/themeConfig';
 import { setTheme } from '@/slices/theme/themeSlice';
+import { FaPalette } from 'react-icons/fa';
 
 const ThemeSwitcher = () => {
     const currentTheme = useSelector((state: RootState) => state.theme.currentTheme);
@@ -16,8 +17,15 @@ const ThemeSwitcher = () => {
     };
 
     return (
-        <div className="theme-switcher">
-            <select value={currentTheme} onChange={handleThemeChange} className="theme-select">
+        <div className="theme-switcher-menu-item">
+            <FaPalette className="menu-icon" />
+            <span>Theme</span>
+            <select 
+                value={currentTheme} 
+                onChange={handleThemeChange} 
+                className="theme-select"
+                aria-label="Select theme"
+            >
                 {THEMES.map((theme) => (
                     <option key={theme} value={theme}>
                         {theme}

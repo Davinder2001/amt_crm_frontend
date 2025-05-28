3a97ab57f657e0580bfa96c553a8b4d6e8ab1b70
neosharma995 <neosharma995@gmail.com>
2025-05-28 19:10:20 +0530

    main page css, login page css

--- src/assets/useImage.tsx
+++ src/assets/useImage.tsx
@@ -2,14 +2,11 @@ import loginImage from './login-image.webp';
 import addRole from './add-role.svg';
 import logo from './logo.png';
 import placeholderImg from './woocommerce-placeholder.png'
-import homeimg1 from './homeimg1.png'
+
 import homeimg2 from './homeimage2.png'
-import homeimg3 from './homeimg3.png'
 import homelogo from './homelogo.png'
 import featurecardimg1 from './featurecardimg1.png'
 import featurecardimg2 from './featurecardimg2.png'
-import featurecardimg3 from './featurecardimg3.png'
-import featurecardimg4 from './featurecardimg4.png'
 import manageimage from './manageimage.png'
 import footerlogoimage from './footerlogoimage.png'
 import loginPageimage from './loginPageimage.png'
@@ -25,5 +22,5 @@ import bottomsvg from './bottomsvg.svg'
 
 
 export {
-    loginImage,headersvg,bottomsvg,adminlogo, addRole, logo, placeholderImg, homeimg1, homeimg2, homeimg3, homelogo, featurecardimg1, featurecardimg2, featurecardimg3, featurecardimg4, manageimage, footerlogoimage, loginPageimage, forgotPageimage, changepass
+    loginImage,headersvg,bottomsvg,adminlogo, addRole, logo, placeholderImg, homeimg2,  homelogo, featurecardimg1, featurecardimg2,  manageimage, footerlogoimage, loginPageimage, forgotPageimage, changepass
 }

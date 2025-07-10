162f06f4688ed7d16a7b7be50b0b62b7b4c24704
Mr_Rakesh <rkrakesh24726@gmail.com>
2025-07-10 18:51:26 +0530

    resolve build errors

--- src/layouts/LayoutWrapper.tsx
+++ src/layouts/LayoutWrapper.tsx
@@ -1,3 +1,41 @@
+// 'use client';
+
+// import { useUser } from '@/provider/UserContext';
+// import layoutMap from './layoutMap';
+// import AuthLayout from '@/layouts/AuthLayout';
+// import Loader from '@/components/common/Loader';
+
+// type LayoutWrapperProps = {
+//   children: React.ReactNode;
+// };
+
+// const LayoutWrapper = ({ children }: LayoutWrapperProps) => {
+//   const { user, authChecked , loadingComplete} = useUser();
+//   const role = user?.user_type;
+
+//   if (!authChecked || !loadingComplete) { 
+//     return <Loader isLoading={!loadingComplete} />; 
+//   }
+
+//   const SelectedLayout = role ? layoutMap[role] : AuthLayout;
+//   return <SelectedLayout>{children}</SelectedLayout>;
+// };
+
+// export default LayoutWrapper;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 'use client';
 
 import { useUser } from '@/provider/UserContext';
@@ -10,11 +48,11 @@ type LayoutWrapperProps = {
 };
 
 const LayoutWrapper = ({ children }: LayoutWrapperProps) => {
-  const { user, authChecked , loadingComplete} = useUser();
+  const { user, authChecked } = useUser();
   const role = user?.user_type;
 
-  if (!authChecked || !loadingComplete) { 
-    return <Loader isLoading={!loadingComplete} />; 
+  if (!authChecked) {
+    return <Loader />;
   }
 
   const SelectedLayout = role ? layoutMap[role] : AuthLayout;
